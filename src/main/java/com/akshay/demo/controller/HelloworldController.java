6907d49da479b37ce4a60f9dd4130f2c1038f805
Akshay Padmanabha <Akshay.Padmanabha@fisglobal.com>
2019-06-11 14:21:48 +0530

    inject config

--- src/main/java/com/akshay/demo/controller/HelloworldController.java
+++ src/main/java/com/akshay/demo/controller/HelloworldController.java
@@ -4,14 +4,17 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.akshay.demo.config.HelloConfig;
+
 @RestController
 public class HelloworldController {
-	@Value("${hello.message}")
-	String message;
+	@Autowired
+	HelloConfig config;
 	
 	@GetMapping("/hello")
     public Collection<String> sayHello() {
@@ -22,6 +25,6 @@ public class HelloworldController {
 	
 	@GetMapping("/hello/message")
 	public String sayHelloMessage() {
-		return message;
+		return config.getMessage();
 	}
 }
