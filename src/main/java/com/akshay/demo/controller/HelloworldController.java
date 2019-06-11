b57710cc3b7f272163de2c50960698473855cea6
Akshay P <akshay.py@gmail.com>
2019-06-11 15:35:04 +0530

    changes

--- src/main/java/com/akshay/demo/controller/HelloworldController.java
+++ src/main/java/com/akshay/demo/controller/HelloworldController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
